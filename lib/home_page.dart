e3eb67db36bc5abc283cdc99d352508e1734df81
CoderSid007 <siddhesdas2020@gmail.com>
2024-08-22 19:41:55 +0530

    PostsModel commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_api/Models/posts_model/PostsModel.dart';
+
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +13,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<PostsModel> postList = [];
+  Future<List<PostsModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      postList.clear();
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +36,49 @@ class _HomePageState extends State<HomePage> {
         title: Text('Api'),
       ),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('Loading');
+                  } else {
+                    return ListView.builder(
+                        itemCount: postList.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Title',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold)),
+                                  Text(postList[index].title.toString()),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    'Description',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text('Description\n' +
+                                      postList[index].title.toString())
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                }),
+          )
+        ],
       ),
     );
   }
